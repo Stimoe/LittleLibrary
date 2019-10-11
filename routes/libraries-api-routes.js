7bd5b29abfb0d59528adcdf096abf1e8512abf2d
Stimoe <stimoe3@gmail.com>
2019-10-11 14:24:39 -0700

    testing stuff

--- routes/libraries-api-routes.js
+++ routes/libraries-api-routes.js
@@ -1,14 +1,14 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/authors", function(req, res) {
+  app.get("/api/libraries", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
-    db.Author.findAll({
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    db.libraries.findAll({
+      // include: [db.Post]
+    }).then(function(dblibraries) {
+      res.json(dblibraries);
     });
   });
 
