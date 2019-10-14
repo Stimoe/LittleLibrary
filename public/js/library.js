ac8769eb8ccd6a34c18adcc6b9d78ed6955de3df
Amjed Ayoub <amjadkayoub@gmail.com>
2019-10-14 11:35:23 -0700

    cleaning files

--- public/js/library.js
+++ public/js/library.js
@@ -6,17 +6,22 @@ $(document).ready(function() {
         if (input !== "") {
             //search by title
             $.get("/api/booksTitle/" + input, function(data) {
-                console.log("books", data);
-                var reslut = $("<h3>");
-                reslut.text(data.title + "\n" + data.author + "\n" + data.genre + "\n" + data.image + "\n" + data.availability + "\n");
-                $(".results").append(reslut);
+                if (data !== null) {
+                    var reslut = $("<h3>");
+                    reslut.text(data.title + "\n" + data.author + "\n" + data.genre + "\n" + data.image + "\n" + data.availability + "\n");
+                    $(".results").append(reslut);
+                }
             });
             //search by genre
             $.get("/api/booksGenre/" + input, function(data) {
-                console.log("books", data);
-                var reslut = $("<h3>");
-                reslut.text(data.title + "\n" + data.author + "\n" + data.genre + "\n" + data.image + "\n" + data.availability + "\n");
-                $(".results").append(reslut);
+                if (data !== null) {
+                    for (let i = 0; i < data.length; i++) {
+                        var reslut = $("<h3>");
+                        reslut.text(data[i].title + "\n" + data[i].author + "\n" + data[i].genre + "\n" + data[i].image + "\n" + data[i].availability + "\n");
+                        $(".results").append(reslut);
+                    }
+
+                }
             });
         }
     });
